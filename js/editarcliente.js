2725c66edd349910a8179fb65dfa9bf6610c1424
soldiersnake <mariano.maciasgandulfo@gmail.com>
2022-05-26 14:53:38 +0200

    primer commit

---
+++ js/editarcliente.js
@@ -0,0 +1,60 @@
+import { editarCliente, obtenerCliente } from "./API.js";
+import { mostrarAlerta, validar } from "./funciones.js";
+
+(function(){
+    // campos del formulario
+    const nombreInput = document.querySelector('#nombre');
+    const emailInput = document.querySelector('#email');
+    const empresaInput = document.querySelector('#empresa');
+    const telefonoInput = document.querySelector('#telefono');
+    const idInput = document.querySelector('#id');
+
+    document.addEventListener('DOMContentLoaded', async () => {
+        const parametrosURL = new URLSearchParams(window.location.search);
+
+        const idCliente = parseInt( parametrosURL.get('id'));
+
+        const cliente = await obtenerCliente(idCliente);
+        
+        mostrarCliente(cliente);
+
+        // submit al formulario
+        const formulario = document.querySelector('#formulario');
+        formulario.addEventListener('submit', validarCliente);
+    })
+
+    function mostrarCliente(cliente){
+        const {nombre, empresa, email, telefono, id } = cliente;
+
+        nombreInput.value = nombre;
+        empresaInput.value = empresa;
+        emailInput.value = email;
+        telefonoInput.value = telefono;
+        idInput.value = id;
+    };
+
+    function validarCliente(e){
+        e.preventDefault();
+
+        const cliente = {
+            nombre: nombreInput.value,
+            email: emailInput.value,
+            telefono: telefonoInput.value,
+            empresa: empresaInput.value,
+            id: parseInt(idInput.value)
+        };
+
+        console.log(cliente);
+
+
+
+        if( validar( cliente ) ){
+            // mostrar mensaje
+            mostrarAlerta('todos los cambios son obligatorios');
+            return;
+        }
+
+        // reescribe el objeto
+        editarCliente(cliente);
+    }
+}());
---
+++ js/nuevocliente.js
@@ -0,0 +1,34 @@
+import { nuevoCliente } from "./API.js";
+import { mostrarAlerta, validar } from "./funciones.js";
+
+
+(function(){
+    const formulario = document.querySelector('#formulario');
+    formulario.addEventListener('submit', validarCliente);
+
+    function validarCliente(e){
+        e.preventDefault();
+
+        const nombre = document.querySelector('#nombre').value;
+        const email = document.querySelector('#email').value;
+        const telefono = document.querySelector('#telefono').value;
+        const empresa = document.querySelector('#empresa').value;
+
+        const cliente = {
+            nombre,
+            email,
+            telefono,
+            empresa
+        };
+
+        if( validar( cliente ) ){
+            // mostrar mensaje
+            mostrarAlerta('todos los cambios son obligatorios');
+            return;
+        }
+        nuevoCliente(cliente);
+    };
+
+
+
+}());
